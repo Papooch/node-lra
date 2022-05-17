bfbcc6c66b5edf76fe49c29229ecc380713f4e81
Ondřej Švanda <papoochcz@gmail.com>
2022-05-17 23:03:59 +0200

    trying to make lra client work

--- apps/express-sample/src/main.ts
+++ apps/express-sample/src/main.ts
@@ -4,11 +4,34 @@
  */
 
 import express from 'express';
+import { LRAClient } from '@node-lra/core';
 
 const app = express();
 
 app.get('/api', (req, res) => {
     res.send({ message: 'Welcome to express-sample!' });
+});
+
+const lraClient = new LRAClient({
+    coordinatorUrl: 'http://localhost:8081/lra-coordinator',
+    compensateUrl: 'http://host.docker.internal/compensate',
+});
+
+app.use((req, res, next) => {
+    console.log('request', req.method, req.path);
+    next();
+});
+
+app.get('/start', async (req, res) => {
+    const result = await lraClient.startLRA('123');
+    res.json({
+        result,
+    });
+});
+
+app.put('/compensate', (req, res) => {
+    console.log('compensating....', req.headers);
+    res.send('compensated');
 });
 
 const port = process.env.port || 3333;
--- packages/core/src/lib/lra-client.ts
+++ packages/core/src/lib/lra-client.ts
@@ -1,11 +1,35 @@
+import axios from 'axios';
+
 export interface LRAClientOptions {
     coordinatorUrl: string;
+    compensateUrl: string;
 }
 
 export class LRAClient {
     private coordinatorUrl: string;
+    private compensateUrl: string;
 
     constructor(options: LRAClientOptions) {
         this.coordinatorUrl = options.coordinatorUrl;
+        this.compensateUrl = options.compensateUrl;
+    }
+
+    async startLRA(clientID: string) {
+        const result = await axios
+            .post(`${this.coordinatorUrl}/start?ClientID=${clientID}`, '', {
+                headers: {
+                    Link: `<${this.compensateUrl}>; rel="compensate"`,
+                },
+            })
+            .catch((reason) => {
+                console.log(reason);
+                return { status: 0, headers: 0, data: 0 };
+            });
+        console.log('headers', result.headers);
+        return {
+            status: result.status,
+            headers: result.headers,
+            body: encodeURIComponent(result.data),
+        };
     }
 }
